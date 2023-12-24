c9173060a42e4d528f6c31f8ab22d795fc99c8bf
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2023-12-23 22:48:59 -0300

    first commit

---
+++ src/main/java/ar/danielayanian/dshop/DshopApplication.java
@@ -0,0 +1,13 @@
+package ar.danielayanian.dshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/danielayanian/dshop/DshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.danielayanian.dshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
