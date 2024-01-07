2c12699ea57d874dd376fe7aed00612e8003c230
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2024-01-07 17:32:50 -0300

    creacion del usuario ADMIN al iniciar la app

--- src/main/java/ar/danielayanian/dshop/DshopApplication.java
+++ src/main/java/ar/danielayanian/dshop/DshopApplication.java
@@ -1,7 +1,12 @@
 package ar.danielayanian.dshop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ar.danielayanian.dshop.entities.User;
+import ar.danielayanian.dshop.repositories.UserRepository;
 
 @SpringBootApplication
 public class DshopApplication {
@@ -10,4 +15,17 @@ public class DshopApplication {
 		SpringApplication.run(DshopApplication.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner demo(UserRepository userRepository) {
+        return (args) -> {
+        	User user = new User();
+        	user.setNombre("Daniel");
+        	user.setApellido("Ayanian");
+        	user.setEmail("dani@gmail.com");
+        	user.setPassword("root");
+        	user.setRoles("ADMIN");
+        	userRepository.save(user);
+        };
+    }
+	
 }
